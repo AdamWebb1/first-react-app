9eb70247d35478ff5a605bf5e07e94740d2ea14b
RandomPro8 <59402575+RandomPro8@users.noreply.github.com>
2023-01-24 16:16:30 -0800

    Started project, got to lesson 4

---
+++ src/TodoContainer.js
@@ -0,0 +1,84 @@
+import React from "react"
+import { v4 as uuidv4 } from "uuid"
+
+import TodosList from "./TodosList"
+import Header from "./Header"
+import InputTodo from "./InputTodo"
+
+import "./App.css"
+
+
+class TodoContainer extends React.Component {
+
+    state = {
+        todos: [
+            {
+                id: uuidv4(),
+                title: "Setup development environment",
+                completed: false
+            },
+            {
+                id: uuidv4(),
+                title: "Develop website and add content",
+                completed: false
+            },
+            {
+                id: uuidv4(),
+                title: "Deploy to live server",
+                completed: false
+            }
+        ]
+       };
+
+    handleChange = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+        })
+    }
+
+    delTodo = (id) => {
+        this.setState({
+            todos: this.state.todos.filter(todo => {
+                return todo.id !== id;
+            })
+        })
+    }
+
+    addTodoItem = title =>{
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false
+        }
+
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
+    }
+
+    render() {
+        return (
+            <div className = "container">
+                <div className = "inner">
+                    <Header 
+                        titleProp={"Todos"}
+                    />
+                    <InputTodo 
+                        addTodoProps={this.addTodoItem}
+                    />
+                    <TodosList 
+                        todos={this.state.todos} 
+                        handleChangeProps={this.handleChange}
+                        delTodoProps={this.delTodo}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+export default TodoContainer
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// Component file
+import TodoContainer from "./TodoContainer"
+
+ReactDOM.render(
+    <React.StrictMode>
+        <TodoContainer />
+    </React.StrictMode>,
+    document.getElementById("root")
+)
